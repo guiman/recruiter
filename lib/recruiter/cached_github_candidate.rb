00e209d1ffdb9713570aaa161c6e36f17c068891
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-03-15 12:51:34 +0000

    Make CachedGithubCandidate composable

--- lib/recruiter/cached_github_candidate.rb
+++ lib/recruiter/cached_github_candidate.rb
@@ -1,18 +1,26 @@
-require 'recruiter/candidate'
+require 'recruiter/github_candidate'
 require 'redis'
 
 module Recruiter
-  class CachedCandidate < Candidate
+  class CachedGithubCandidate
+    def initialize(candidate)
+      @candidate = candidate
+    end
+
     def self.redis
       @redis ||= ::Redis.new
     end
 
+    def method_missing(name)
+      @candidate.public_send(name)
+    end
+
     def all_repositories
-      redis_cache_key = "#{login}_all_repositories"
+      redis_cache_key = "#{@candidate.login}_all_repositories"
       if repositories = self.class.redis.get(redis_cache_key)
         cached_repositories = Marshal.load(repositories)
       else
-        repositories = super
+        repositories = @candidate.all_repositories
         self.class.redis.set(redis_cache_key, Marshal.dump(repositories))
         cached_repositories = repositories
       end
@@ -21,11 +29,11 @@ module Recruiter
     end
 
     def languages
-      redis_cache_key = "#{login}_languages"
+      redis_cache_key = "#{@candidate.login}_languages"
       if languages = self.class.redis.get(redis_cache_key)
         cached_languages = Marshal.load(languages)
       else
-        languages = super
+        languages = @candidate.languages
         self.class.redis.set(redis_cache_key, Marshal.dump(languages))
         cached_languages = languages
       end
@@ -34,11 +42,11 @@ module Recruiter
     end
 
     def email
-      redis_cache_key = "#{login}_email"
+      redis_cache_key = "#{@candidate.login}_email"
       if email = self.class.redis.get(redis_cache_key)
         cached_email = Marshal.load(email)
       else
-        email  = super
+        email  = @candidate.email
         self.class.redis.set(redis_cache_key, Marshal.dump(email))
         cached_email = email
       end
--- lib/recruiter/cached_search_strategy.rb
+++ lib/recruiter/cached_search_strategy.rb
@@ -1,4 +1,5 @@
 require 'recruiter/github_search_strategy'
+require 'recruiter/cached_github_candidate'
 require 'redis'
 
 module Recruiter
@@ -11,13 +12,17 @@ module Recruiter
       @redis ||= ::Redis.new
     end
 
+    def model
+      ::Recruiter::CachedGithubCandidate
+    end
+
     def all(search)
       redis_cache_key = search
 
       if cached_search = self.class.redis.get(redis_cache_key)
         cached_search = Marshal.load(cached_search)
       else
-        search_results = @composite.all(search)
+        search_results = @composite.all(search).map { |candidate| model.new(candidate) }
         self.class.redis.set(redis_cache_key, Marshal.dump(search_results))
         cached_search = search_results
       end
